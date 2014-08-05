ea7acbf4925b1e85a01d1d0d1ab1a37fb4a9f6a4
FENG-HSIANG Chien <fred@FENG-HSIANGdeMacBook-Pro.local>
2014-08-05 09:00:25 +0800

    Initial version

--- iOS-WeatherApp/ViewController.swift
+++ iOS-WeatherApp/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var weatherLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    
+    let urlPath: String = "http://api.openweathermap.org/data/2.5/weather?q=Taipei"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getWeather()
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func getWeather() {
+        var url: NSURL = NSURL(string: urlPath)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+            
+            if (error) {
+                println("Cannot get weather information from server.")
+                return
+            }
+            
+            // Parsing JSON
+            var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+            
+            // Display
+            let temp : AnyObject? = jsonResult["main"]?["temp"]
+            self.temperatureLabel.text = "\(Int(round(temp!.floatValue - 273.15)))℃"
+            
+            let weather : AnyObject? = jsonResult["weather"]?[0]!["description"]
+            self.weatherLabel.text = "\(weather!)"
+            
+            println(jsonResult)
+        }
+    }
 
 }
 
