dd4e294e890d42b65b90b0c22e7eb91c7424397f
tonebeta <apple@appledeMacBook-Pro.local>
2014-08-17 19:14:54 +0800

    add icon

--- iOS-WeatherApp/ViewController.swift
+++ iOS-WeatherApp/ViewController.swift
@@ -12,13 +12,34 @@ class ViewController: UIViewController {
     
     @IBOutlet var weatherLabel: UILabel!
     @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var cityName: UITextField!
+    @IBOutlet var cityNameLabel: UILabel!
+    @IBOutlet var image: UIImageView!
     
-    let urlPath: String = "http://api.openweathermap.org/data/2.5/weather?q=Taipei"
+    var urlPath: String = "http://api.openweathermap.org/data/2.5/weather?q="
+    
+    @IBAction func actionGetCityName(sender: AnyObject) {
+        
+        urlPath = "http://api.openweathermap.org/data/2.5/weather?q="
+        
+        urlPath += cityName.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        
+        println(urlPath)
+        
+        if urlPath != "http://api.openweathermap.org/data/2.5/weather?q=" {
+            
+            getWeather()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        getWeather()
+        
+        let background = UIImage(named: "rain-icon.png")
+        
+        self.view.backgroundColor = UIColor(patternImage : background)
+        
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -45,15 +66,23 @@ class ViewController: UIViewController {
             var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
             
             // Display
-            let temp : AnyObject? = jsonResult["main"]?["temp"]
+            
+            var city : AnyObject? = jsonResult["name"]?
+            self.cityNameLabel.text = "\(city!)"
+            
+            var icon = jsonResult["weather"]?[0]!["icon"]
+            self.image.image =  UIImage(data: NSData(contentsOfURL: NSURL(string:"http://www.openweathermap.org/img/w/\(icon!).png")))
+            
+            var temp : AnyObject? = jsonResult["main"]?["temp"]
             self.temperatureLabel.text = "\(Int(round(temp!.floatValue - 273.15)))℃"
             
-            let weather : AnyObject? = jsonResult["weather"]?[0]!["description"]
+            var weather : AnyObject? = jsonResult["weather"]?[0]!["description"]
             self.weatherLabel.text = "\(weather!)"
+            
             
             println(jsonResult)
         }
     }
-
+    
 }
 
